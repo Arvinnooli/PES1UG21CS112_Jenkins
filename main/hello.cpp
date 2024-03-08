0a7511e789044fcb2b85f884760b875defb58e72
Arvin Nooli <arvinnooli711@gmail.com>
2024-03-09 00:56:32 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,8 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES1UG21CS112\n";
+  cout << "PES1UG21CS112 trial to see what happens\n";
+
   cout << "I have successfully built and run\n";
   return 0;
 }
