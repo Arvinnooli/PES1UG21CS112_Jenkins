f556f0b01531f11a18b6915f560319f5c4b888a9
Arvin Nooli <arvinnooli711@gmail.com>
2024-03-09 01:25:07 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,8 +5,15 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "hahahahhahaaa\n";
 
+
+
+
+
+
+
+
+  
   cout << "I have successfully built and run\n";
   return 0;
 }
