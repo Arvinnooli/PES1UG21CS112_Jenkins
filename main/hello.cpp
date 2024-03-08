f70fc17346765beb554ddf70c99f9e3b7d4991db
Arvin Nooli <arvinnooli711@gmail.com>
2024-03-09 01:11:35 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES1UG21CS112\n";
-  cout << "PES1UG21CS112 trial to see what happens\n";
+  cout << "hahahahhahaaa\n";
 
   cout << "I have successfully built and run\n";
   return 0;
